17b9bda311dbdc7f667fe258c898f69b5cc256f9
oussamaZAGHDOUD <oussamazaghdoud1991@gmail.com>
2021-01-03 20:01:16 +0100

    empty project test commit

---
+++ RestaurantDS/src/main/java/com/OZ/RestaurantDsApplication.java
@@ -0,0 +1,13 @@
+package com.OZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantDsApplication.class, args);
+	}
+
+}
---
+++ RestaurantDS/src/test/java/com/OZ/RestaurantDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
